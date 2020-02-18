361cc1c0af1f391e729762560a009729e3db147b
Paolo Arduin <paolo.arduin@errequadrosrl.com>
2020-02-18 17:09:33 +0100

    Fixed bump version in setup.py

--- setup.py
+++ setup.py
@@ -4,43 +4,43 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = []
 
-setup_requirements = ['pytest-runner']
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest']
+test_requirements = ["pytest"]
 
 setup(
     author="Paolo Arduin",
-    author_email='paolo.arduin@errequadrosrl.com',
-    python_requires='>=3.5',
+    author_email="paolo.arduin@errequadrosrl.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="A set of custom spaCy pipeline components based on regex.",
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='respacy',
-    name='respacy',
-    packages=find_packages(include=['respacy', 'respacy.*']),
+    keywords="respacy",
+    name="respacy",
+    packages=find_packages(include=["respacy", "respacy.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/erre-quadro/respacy',
-    version='version='0.1.0'',
+    url="https://github.com/erre-quadro/respacy",
+    version="0.1.0",
     zip_safe=False,
 )
