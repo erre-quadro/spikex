20f43dc61032bff3e40bf659f0a7902b74c57054
Paolo Arduin <paolo.arduin@errequadrosrl.com>
2020-02-18 17:40:21 +0100

    Fixed sphinx conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ import respacy
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
---
+++ respacy/__init__.py
@@ -0,0 +1,1 @@
+from ._about import __version__
