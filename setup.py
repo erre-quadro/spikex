042e185c49ad8cf6cf911dd1b7b3bfdc5fbafd98
Paolo Arduin <paolo.arduin@errequadrosrl.com>
2020-03-25 15:00:17 +0100

    Bump version: 0.2.1-dev2 → 0.2.2-dev0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/erre-quadro/respacy",
-    version="0.2.1-dev2",
+    version="0.2.2-dev0",
     zip_safe=False,
 )
