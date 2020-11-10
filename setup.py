8ebfb54cf821429065d63f3b1b39893c93f1aefa
Paolo Arduin <paolo.arduin@errequadrosrl.com>
2020-11-10 13:14:49 +0100

    Bump version: 0.3.0 → 0.3.0-01

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/erre-quadro/spikex",
-    version="0.3.0",
+    version="version="0.3.0"",
     zip_safe=False,
 )
