0d27aeacb82fa4d7f52263270dcbf8d6cd688415
Paolo Arduin <paolo.arduin@errequadrosrl.com>
2020-11-10 16:13:34 +0100

    Fix version

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/erre-quadro/spikex",
-    version="version="0.3.0"",
+    version="0.3.0",
     zip_safe=False,
 )
